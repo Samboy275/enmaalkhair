1c6654483fdadfc8c8bdb9861a6002f7834c1b8e
samboy275 <sam-wal@windowslive.com>
2026-04-29 23:25:11 +0300

    Production settings

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'https://enmaalkhair1.pythonanywhere.com/']
 
 
 # Application definition
