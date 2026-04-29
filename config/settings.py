f50d0980524ccbd272bd5e63cf5174f0cf6c48d0
samboy275 <sam-wal@windowslive.com>
2026-04-29 19:29:46 +0300

    Fix apps names in config

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'apps.branding',
+    'apps.legal',
 ]
 
 MIDDLEWARE = [
