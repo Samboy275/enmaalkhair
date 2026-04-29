9675dd38d27b5c6333231a4860f8b2a4fe04bca8
samboy275 <sam-wal@windowslive.com>
2026-04-29 19:23:38 +0300

    Add the urls files

--- apps/branding/models.py
+++ apps/branding/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class LegalDocument(models.Model):
+    """ Model to save legal documents for the user """
+
+    TYPE_CHOICES = [
+        ('privacy', 'Privacy Terms'),
+        ('conditions', 'Terms and Conditions')
+    ]
+
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=15, choices=TYPE_CHOICES)
+    text = models.TextField()
+    is_active = models.BooleanField(default=False)
+
