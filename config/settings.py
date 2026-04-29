6745168345dd138cb9c1836f87bf80c683375a07
samboy275 <sam-wal@windowslive.com>
2026-04-29 23:44:15 +0300

    Fix issue with none scorlling body

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', 'enmaalkhair1.pythonanywhere.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'enmaalkhair1.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
